59bdf46ec9cbced54a9652edb012b68f089ce6a9
Danet-y <83012523+Danet-y@users.noreply.github.com>
2023-06-30 13:01:59 -0300

    Create لانه کبوتری.cpp

---
+++ Year1402/لانه کبوتری.cpp
@@ -0,0 +1,20 @@
+// IN THE NAME OF GOD
+#include<bits/stdc++.h>
+#define endl '\n'
+#define file_reading freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define flush cout.flush();
+using namespace std;
+typedef long long ll;
+typedef long double dll;
+typedef unsigned long long ull;
+
+int32_t main(){
+     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	int n,m; cin >> n >> m; 
+	if (n > m) cout << "Yes" << endl;
+	else cout << "No" << endl; 
+
+	return 0;
+}
+//Iliya AGHAZADEH
