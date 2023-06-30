d88ca50361283350156fd0fb89dd0583a179e13f
Danet-y <83012523+Danet-y@users.noreply.github.com>
2023-06-30 13:07:57 -0300

    By iliyaAghazadeh

--- Year1402/لانه کبوتری.cpp
+++ Year1402/لانه کبوتری.cpp
@@ -7,7 +7,7 @@ using namespace std;
 typedef long long ll;
 typedef long double dll;
 typedef unsigned long long ull;
-
+ 
 int32_t main(){
      ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
